2849311faa17d116c167ede2df4e95adc11fa397
NikitaRybakov21 <nikitarybakov044@gmail.com>
2021-07-18 19:36:24 +0300

    DZ8_LESSON8

--- src/main/java/com/github/Human.java
+++ src/main/java/com/github/Human.java
@@ -1,5 +1,28 @@
 package com.github;
 
 public class Human implements Player{
+    private String name;
+    private int maxRun;
+    private int maxJump;
 
+    public Human(String name, int maxRun, int maxJump) {
+        this.name = name;
+        this.maxRun = maxRun;
+        this.maxJump = maxJump;
+    }
+
+    @Override
+    public boolean run(int length) {
+        return this.maxRun >= length;
+    }
+
+    @Override
+    public boolean jump(int height) {
+        return this.maxJump >= height;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
 }
